c0e26bff8bfb2e19f4274b8062d7bb27610b3ea3
esatekh <satej.khandeparkar@ericsson.com>
2017-08-29 11:36:48 +0200

    Battleship in cpp

---
+++ Battleship_cpp/Battleship_avenger/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char **argv)
+{
+    
+    cout<<"Battleship: Avenge of the seven seas"<<endl;
+    
+    
+    
+    
+	return 0;
+}
+
+
