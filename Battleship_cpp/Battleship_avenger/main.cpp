b2356f5325b891ecce01bcf77116fa4956e6b8a2
esatekh <satej.khandeparkar@ericsson.com>
2017-08-29 12:03:23 +0200

    New updatte

--- Battleship_cpp/Battleship_avenger/main.cpp
+++ Battleship_cpp/Battleship_avenger/main.cpp
@@ -3,13 +3,33 @@
 using namespace std;
 
 
+class Box{
+    
+public:
+    
+    double length;
+    double breadth;
+    double height;
+    
+    
+};
+    
+
 
 int main(int argc, char **argv)
 {
     
     cout<<"Battleship: Avenge of the seven seas"<<endl;
     
+    Box box1;
+
+    double area = 0;
     
+    box1.length = 2.0;
+    box1.breadth = 3.0;
+    
+    area = box1.length * box1.length;
+    cout<<"area = "<< area<<endl;
     
     
 	return 0;
